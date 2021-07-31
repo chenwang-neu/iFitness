dad73aed0b6cddaff3926bd2f1089651e0311411
chen wang <wangchen.neu@gmail.com>
2021-07-31 18:41:24 -0400

    tracker initial commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends AppCompatActivity {
     private Button button;
     @Override
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openActivity() {
-        Intent intent = new Intent(this, MapsActivity.class);
+        Intent intent = new Intent(this, RunningTrackerActivity.class);
         startActivity(intent);
     }
 }
