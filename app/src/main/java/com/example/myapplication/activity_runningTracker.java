6f6803cb253bf61aaaa1c14850f4dae1c10c20a2
Yuanyuan Zhou <zhou.yuan@northeastern.edu>
2021-07-31 20:28:37 -0400

    update running tracker page.

--- app/src/main/java/com/example/myapplication/activity_runningTracker.java
+++ app/src/main/java/com/example/myapplication/activity_runningTracker.java
@@ -1,14 +1,53 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+
+import org.jetbrains.annotations.NotNull;
 
 public class activity_runningTracker extends AppCompatActivity {
+//    private LocationManager locationManager;
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running_tracker);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+
+            }
+        });
+
+//        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//        locationManager.getCurrentLocation();
+//
+//        TextView speedText = findViewById(R.id.speedNum);
+    }
+
+    public void openNewPlan(View view) {
+        Intent intent = new Intent(this, CreateNewPlanActivity.class);
+        startActivity(intent);
+    }
+
+    public void openToday(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
