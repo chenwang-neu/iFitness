9b0dc243a5417e64b3e98cfd98015d2dfbfc35e7
kailin0216 <jin.ka@northeastern.edu>
2021-08-09 19:05:12 -0700

    update

--- app/src/main/java/com/example/myapplication/current_schedule/ScheduledPlanActivity.java
+++ app/src/main/java/com/example/myapplication/current_schedule/ScheduledPlanActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.current_schedule;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -16,13 +18,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.example.myapplication.RunningTrackerActivity;
+import com.example.myapplication.model.Calendar;
+import com.example.myapplication.model.Exercise;
 import com.example.myapplication.newplan.DayItem;
 import com.example.myapplication.newplan.NewPlanActivity;
 import com.example.myapplication.newplan.WorkoutItem;
+import com.example.myapplication.service.DataBaseHelper;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ScheduledPlanActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -31,34 +43,42 @@ public class ScheduledPlanActivity extends AppCompatActivity implements AdapterV
     public RecyclerView mRecyclerView;
     public RecyclerView.LayoutManager mLayoutManager;
     public TaskRecycleViewAdapter mAdapter;
-    public ArrayList<WorkoutItem> todayTaskList = new ArrayList<>();
+    public ArrayList<WorkoutItem> todayTaskList;
     public AlertDialog.Builder dialogBuilder;
     public AlertDialog dialog;
     public Button gotItBtn;
     public ImageButton infoButton;
     private ArrayList<DayItem> weekdayItemList;
     private DayItemAdapter dayAdapter;
+
+
+
+    //chou test here for database
+    List<Exercise> exerciseList = new ArrayList<>();
+    //List<Calendar> selectedDayExerciseList = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scheduled_plan);
         displayDateAndTime();
-        buildRecyclerView();
         initSpinner();
-
         // to be added - change checkbox status by database info.
         // handle screen rotation
+        //chou test here for database
+        //Log.d("The exerciselist at 0 is:", String.valueOf(exerciseList.size()));
 
     }
 
     public void fillDayItem(){
-        mon = new DayItem("Monday", Boolean.FALSE, findViewById(R.id.mondayBtn));
-        tue = new DayItem("Tuesday", Boolean.FALSE, findViewById(R.id.tuesdayBtn));
-        wed = new DayItem("Wednesday", Boolean.FALSE, findViewById(R.id.wednesdayBtn));
-        thurs = new DayItem("Thursday", Boolean.FALSE, findViewById(R.id.thursdayBtn));
-        fri = new DayItem("Friday", Boolean.FALSE, findViewById(R.id.fridayBtn));
-        sat = new DayItem("Saturday", Boolean.FALSE, findViewById(R.id.saturdayBtn));
-        sun = new DayItem("Sunday", Boolean.FALSE, findViewById(R.id.sundayBtn));
+        mon = new DayItem("MON", Boolean.FALSE, findViewById(R.id.mondayBtn));
+        tue = new DayItem("TUES", Boolean.FALSE, findViewById(R.id.tuesdayBtn));
+        wed = new DayItem("WED", Boolean.FALSE, findViewById(R.id.wednesdayBtn));
+        thurs = new DayItem("THUR", Boolean.FALSE, findViewById(R.id.thursdayBtn));
+        fri = new DayItem("FRI", Boolean.FALSE, findViewById(R.id.fridayBtn));
+        sat = new DayItem("SAT", Boolean.FALSE, findViewById(R.id.saturdayBtn));
+        sun = new DayItem("SUN", Boolean.FALSE, findViewById(R.id.sundayBtn));
 
         weekdayItemList = new ArrayList<DayItem>();
         weekdayItemList.add(mon);
@@ -77,10 +97,64 @@ public class ScheduledPlanActivity extends AppCompatActivity implements AdapterV
         weekdaySpinner.setAdapter(dayAdapter);
         weekdaySpinner.setOnItemSelectedListener(this);
     }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String selectedDay = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), selectedDay, Toast.LENGTH_SHORT).show();
+        todayTaskList = new ArrayList<>();
+        List<String> selectedExerciseList = new ArrayList<>();
+        List<Calendar> selectedDayExerciseList = new ArrayList<>();
+
+        DayItem selectedDay = (DayItem) parent.getItemAtPosition(position);
+
+        // get calendar list for selectedDay:
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(ScheduledPlanActivity.this);
+
+        selectedDayExerciseList = dataBaseHelper.getCalendarByDay(selectedDay.getWeekday());
+        Log.d("getCalendarByDay is !!!!!!!!!!!!!!!", String.valueOf(selectedDayExerciseList.toString()));
+
+        for (Calendar s : selectedDayExerciseList) {
+            if (s.getCname().equals(selectedDay.getWeekday())) {
+                selectedExerciseList.add(s.getEname());
+            }
+        }
+        Log.d("Exercises list include !!!!!!!!!!!!!!!", String.valueOf(selectedExerciseList.toString()));
+
+
+        //exercises for the day are in selectedExercisesList, then find them from firebase and show them in the frontpage
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("exercises");
+        reference.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Exercise exercise = snapshot.getValue(Exercise.class);
+
+                    if (selectedExerciseList.contains(exercise.getEname())) {
+                      //  Log.d("exercise content is !!!!!!!!!!!!!1:", exercise.toString());
+//                        //add exercise to todayTaskList
+//                        //todayTaskList.add(0, new WorkoutItem("Swim", Boolean.FALSE, null, 100,
+////                "famous activity swim"));
+                        String ename = exercise.getEname();
+                        int calories = exercise.getCalories();
+                        String description = exercise.getDescription();
+                        WorkoutItem workoutItem = new WorkoutItem(ename, Boolean.FALSE, null, calories, description);
+                        todayTaskList.add(workoutItem);
+//                    }
+                    }
+                }
+                buildRecyclerView();
+                Log.d("The exerciseList is !!!!!!!!!!!!!!!!!!!!!!", String.valueOf(exerciseList.size()));
+//                selectedExerciseList.clear();
+//                exerciseList.clear();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     @Override
@@ -98,22 +172,22 @@ public class ScheduledPlanActivity extends AppCompatActivity implements AdapterV
     }
 
     // subject to change
-    public void fillRecyclerView(){
-        todayTaskList.add(0, new WorkoutItem("Swim", Boolean.FALSE, null, 100,
-                "famous activity swim"));
-        todayTaskList.add(0, new WorkoutItem("Push ups", Boolean.FALSE, null, 100, "too difficult" +
-                " to me "));
-        todayTaskList.add(0, new WorkoutItem("Dance", Boolean.FALSE, null, 100, "i love dance!"));
-        todayTaskList.add(0, new WorkoutItem("Jump", Boolean.FALSE, null, 100, "jump 1000 times " +
-                "a day"));
-        todayTaskList.add(0, new WorkoutItem("Basketball", Boolean.FALSE, null, 100, "wish i " +
-                "could be taller"));
-        todayTaskList.add(0, new WorkoutItem("Football", Boolean.FALSE, null, 100, "run run run"));
-        todayTaskList.add(0, new WorkoutItem("Tennis", Boolean.FALSE, null, 100, "Tennis is a racket sport that can be played individually against a single opponent (singles) or between two teams of two players each (doubles). Each player uses a tennis racket that is strung with cord to strike a hollow rubber ball covered with felt over or around a net and into the opponent's court. "));
-    }
+//    public void fillRecyclerView(){
+//        todayTaskList.add(0, new WorkoutItem("Swim", Boolean.FALSE, null, 100,
+//                "famous activity swim"));
+//        todayTaskList.add(0, new WorkoutItem("Push ups", Boolean.FALSE, null, 100, "too difficult" +
+//                " to me "));
+//        todayTaskList.add(0, new WorkoutItem("Dance", Boolean.FALSE, null, 100, "i love dance!"));
+//        todayTaskList.add(0, new WorkoutItem("Jump", Boolean.FALSE, null, 100, "jump 1000 times " +
+//                "a day"));
+//        todayTaskList.add(0, new WorkoutItem("Basketball", Boolean.FALSE, null, 100, "wish i " +
+//                "could be taller"));
+//        todayTaskList.add(0, new WorkoutItem("Football", Boolean.FALSE, null, 100, "run run run"));
+//        todayTaskList.add(0, new WorkoutItem("Tennis", Boolean.FALSE, null, 100, "Tennis is a racket sport that can be played individually against a single opponent (singles) or between two teams of two players each (doubles). Each player uses a tennis racket that is strung with cord to strike a hollow rubber ball covered with felt over or around a net and into the opponent's court. "));
+//    }
 
     public void buildRecyclerView(){
-        fillRecyclerView();
+        //fillRecyclerView();
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView = findViewById(R.id.todayListRecycler);
         mRecyclerView.setHasFixedSize(true);
@@ -167,10 +241,10 @@ public class ScheduledPlanActivity extends AppCompatActivity implements AdapterV
     }
 
 
-//    public void openRunningTracker(View view) {
-//        Intent intent = new Intent(this, activity_runningTracker.class);
-//        startActivity(intent);
-//    }
+    public void openRunningTracker(View view) {
+        Intent intent = new Intent(this, RunningTrackerActivity.class);
+        startActivity(intent);
+    }
 
     public void openNewPlan(View view) {
         Intent intent = new Intent(this, NewPlanActivity.class);
